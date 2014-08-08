d0d4d60f8e5a3f149732d8c6b612e7353f84cad1
ByteBlast <byteblast@gmail.com>
2014-08-08 10:38:37 +0100

    Implemented ScreenshotProvider.

---
+++ Source/Screenshot/ScreenshotProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using Screenshot.Windows;
+
+namespace Screenshot
+{
+    public static class ScreenshotProvider
+    {
+        public static Bitmap TakeScreenshot(Rectangle area)
+        {
+            // Use the Windows API here instead of the equivalent managed API as the managed 
+            // equivilant has a number of problems.
+
+            IntPtr handleDesktopWindow = NativeMethods.GetDesktopWindow();
+            IntPtr handleSource = NativeMethods.GetWindowDC(handleDesktopWindow);
+            IntPtr handleDestination = NativeMethods.CreateCompatibleDC(handleSource);
+            IntPtr handleBitmap = NativeMethods.CreateCompatibleBitmap(handleSource, area.Width, area.Height);
+            IntPtr handleOldBitmap = NativeMethods.SelectObject(handleDestination, handleBitmap);
+
+            NativeMethods.BitBlt(handleDestination, 0, 0, area.Width, area.Height, handleSource, area.X, area.Y, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+
+            Bitmap desktopCapture = Image.FromHbitmap(handleBitmap);
+            NativeMethods.SelectObject(handleDestination, handleOldBitmap);
+            NativeMethods.DeleteObject(handleBitmap);
+            NativeMethods.DeleteDC(handleDestination);
+            NativeMethods.ReleaseDC(handleDesktopWindow, handleSource);
+
+            return desktopCapture;
+        }
+    }
+}
---
+++ Source/Screenshot/Windows/NativeMethods.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Screenshot.Windows
+{
+    internal static class NativeMethods
+    {
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetDC(IntPtr windowHandle);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetDesktopWindow();
+
+        [DllImport("user32.dll")]
+        internal static extern bool ReleaseDC(IntPtr windowHandle, IntPtr dcHandle);
+
+        [DllImport("user32.dll")]
+        internal static extern IntPtr GetWindowDC(IntPtr windowHandle);
+
+        [DllImport("gdi32.dll")]
+        internal static extern IntPtr CreateCompatibleDC(IntPtr dcHandle);
+
+        [DllImport("gdi32.dll")]
+        internal static extern bool DeleteDC(IntPtr dcHandle);
+
+        [DllImport("gdi32.dll")]
+        internal static extern bool DeleteObject(IntPtr objectHandle);
+
+        [DllImport("gdi32.dll")]
+        internal static extern IntPtr SelectObject(IntPtr dcHandle, IntPtr objectHandle);
+
+        [DllImport("gdi32.dll")]
+        internal static extern IntPtr CreateCompatibleBitmap(IntPtr dcHandle, int width, int height);
+
+        [DllImport("gdi32.dll")]
+        internal static extern bool BitBlt(
+            IntPtr destinationDcHandle,
+            int destinationX,
+            int destinationY,
+            int width,
+            int height,
+            IntPtr sourceDcHandle,
+            int sourceX,
+            int sourceY,
+            CopyPixelOperation rasterOperation);
+    }
+}
