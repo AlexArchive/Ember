62fad4ebf1ca10e46b7cddb9de8b97c516cd8d24
ByteBlast <byteblast@gmail.com>
2014-08-08 17:32:12 +0100

    Image is now uploaded to Imgur after capture.

--- Source/Screenshot/Forms/MainForm.cs
+++ Source/Screenshot/Forms/MainForm.cs
@@ -1,16 +1,58 @@
-﻿using System.Windows.Forms;
+﻿using System.Diagnostics;
 using Shortcut;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows.Forms;
 
 namespace Screenshot.Forms
 {
     public partial class MainForm : Form
     {
-        private HotkeyBinder binder = new HotkeyBinder();
+        private readonly HotkeyBinder binder = new HotkeyBinder();
 
         public MainForm()
         {
             InitializeComponent();
-            binder.Bind(Modifiers.Control,Keys.A).To(()=> new SelectAreaDialog().ShowDialog());
+            InitializeHotkeyBinder();
+        }
+
+        private void InitializeHotkeyBinder()
+        {
+            binder.Bind(Modifiers.Control, Keys.A).To(CaptureArea);
+        }
+
+        private void CaptureArea()
+        {
+            var dialog = new SelectAreaDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Capture(dialog.SelectedArea);
+            }
+
+            dialog.Dispose();
+        }
+
+        private async void Capture(Rectangle area)
+        {
+            var screenshot = ScreenshotProvider.TakeScreenshot(area);
+            var screenshotBinary = ToByteArray(screenshot, ImageFormat.Png);
+
+            using (var client = new ImgurClient())
+            {
+                var imageLink = await client.UploadImageAsync(screenshotBinary);
+                Process.Start(imageLink);
+            }
+        }
+
+        public static byte[] ToByteArray(Image image, ImageFormat format)
+        {
+            using(var memoryStream = new MemoryStream())
+            {
+                image.Save(memoryStream, format);
+                return memoryStream.ToArray();
+            }
         }
 
         protected override void SetVisibleCore(bool value)
---
+++ Source/Screenshot/ImgurClient.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+
+namespace Screenshot
+{
+    public class ImgurClient : IDisposable
+    {
+        private readonly HttpClient client;
+        private const string ClientId = "771cb62f3057260";
+
+        public ImgurClient()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("https://api.imgur.com/3/");
+            client.DefaultRequestHeaders.Add(
+                "Authorization", "Client-ID " + ClientId);
+        }
+
+        public async Task<string> UploadImageAsync(byte[] imageData)
+        {
+            var content = new ByteArrayContent(imageData);
+
+            var response = await client.PostAsync("image", content);
+            response.EnsureSuccessStatusCode();
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var model = new JavaScriptSerializer().Deserialize<dynamic>(responseContent);
+            var imageLink = model["data"]["link"];
+
+            return imageLink;
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
